691958ae876947a887f83e0bc28d55c2d9835cba
Hajrash12 <awalmohammedhajara@gmail.com>
2023-05-31 04:10:15 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form');
+let fullname = document.querySelector('#name');
+let email = document.querySelector('#email');
+const error = document.querySelector('.error');
+const btn = document.querySelector('.btn');
+
+function vanish() {
+    error.style.display = 'none';
+}
+
+btn.addEventListener('click', function(e){
+    e.preventDefault();
+
+    if(fullname.value === '' ||  email.value === ''){
+        error.style.display = 'block';
+        setTimeout(vanish, 5000)
+        return;
+    }
+    let result = `Hello ${fullname.value}🎊👨‍💻,  
+                your form has been submitted successfully. Your email is ${email.value}`
+    alert(result);
+
+    fullname.value = ''
+    email.value = ''
+    
+});
