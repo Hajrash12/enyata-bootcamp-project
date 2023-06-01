d6f05d00d123f438fcce62236f3b6e9e7d81cf14
Hajrash12 <awalmohammedhajara@gmail.com>
2023-05-31 18:00:15 -0700

    final project

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-let form = document.querySelector('form');
 let fullname = document.querySelector('#name');
 let email = document.querySelector('#email');
 const error = document.querySelector('.error');
 const btn = document.querySelector('.btn');
 
-function vanish() {
+function disappear() {
     error.style.display = 'none';
 }
 
@@ -13,14 +12,13 @@ btn.addEventListener('click', function(e){
 
     if(fullname.value === '' ||  email.value === ''){
         error.style.display = 'block';
-        setTimeout(vanish, 5000)
+        setTimeout(disappear, 5000)
         return;
     }
-    let result = `Hello ${fullname.value}🎊👨‍💻,  
-                your form has been submitted successfully. Your email is ${email.value}`
+    let result = `Hello ${fullname.value}🎊👨‍💻, your form has been submitted successfully. Your email is ${email.value}`
     alert(result);
 
-    fullname.value = ''
-    email.value = ''
+    fullname.value = '';
+    email.value = '';
     
 });
